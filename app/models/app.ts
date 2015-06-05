3f7f22c8cfe918790b2fcd053ba7c6263ae3a1ba
Petr Hanc <hanc@cngroup.dk>
2015-06-05 12:26:19 +0200

    TechsService v1

--- app/models/app.ts
+++ app/models/app.ts
@@ -1,11 +1,14 @@
 /// <reference path='../typings/angularjs/angular.d.ts' />
 /// <reference path='config/routes.ts' />
+/// <reference path='controller.ts' />
 
 
 module demoApp {
     'use strict';
 
     angular
-        .module('hakkaStack', ['ui.router'])
-        .config(demoApp.config.routesConfig);
+        .module('hakkaStack', ['ui.router', 'restangular'])
+        .config(demoApp.config.routesConfig)
+        .controller('DemoController', DemoController)
+        .service('TechsService', TechsService);
 }
---
+++ app/models/techsService.ts
@@ -0,0 +1,27 @@
+/// <reference path='../typings/restangular/restangular.d.ts' />
+
+module demoApp {
+    'use strict';
+
+    export class TechsService {
+
+        // $inject annotation.
+        // It provides $injector with information about dependencies to be injected into constructor
+        // it is better to have it close to the constructor, because the parameters must match in count and type.
+        // See http://docs.angularjs.org/guide/di
+        public static $inject = [
+            'Restangular'
+        ];
+
+        // dependencies are injected via AngularJS $injector
+        // controller's name is registered in Application.ts and specified from ng-controller attribute in index.html
+        constructor(private Restangular: restangular.IService) {
+        }
+
+        getAllTechs() {
+            return this.Restangular.allUrl('techs', 'http://hakkastack.apiblueprint.org/techs').getList();
+        }
+
+    }
+
+}
